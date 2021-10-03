5dd4d618472895bc40ab0dbb0dbc1990d16d0cae
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-10-03 18:11:22 -0500

    feat: Habilita modulo cv
    Se habilita modulo cv que dejó de funcionar en el cambio a netlify

---
+++ R/cv_andres_felipe_insuasty_ch.R
@@ -0,0 +1,11 @@
+# cv Andrés Insuasty
+
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Andrés Felipe Insuasty Ch.",
+  data_location = "https://docs.google.com/spreadsheets/d/1jlcKboGFXHr2BKBhP78CkQomaAQ59M3iYRM9s1CGQJg/edit?usp=sharing",
+  pdf_location = "https://github.com/ainsu23/cv/strayer_cv.pdf",
+  html_location = "ainsuasty.com/cv/",
+  source_location = "https://github.com/ainsu23/resume"
+)
+
